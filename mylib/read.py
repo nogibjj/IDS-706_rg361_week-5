1589d0bb7826d22ada7287fa59d6d4bbc44d0e87
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-10-04 05:02:22 +0000

    read query

--- mylib/read.py
+++ mylib/read.py
@@ -4,14 +4,20 @@ File to read data from a DB
 
 import sqlite3
 
-def read_db(db_name="Master.db", table_name="Master", columns="*"):
+def read_db(db_name="Master.db", table_name="Master", query=None):
     """
-    Read from a DB
+    Read from a DB, if no query is provided, it will return the first 5 rows
     """
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    c.execute("SELECT {} FROM {}".format(columns, table_name))
-    payload = c.fetchall()
+    if query==None:
+        query = "SELECT * FROM "+table_name +" LIMIT 5;"
+
+    try:
+        c.execute(query)
+        payload = c.fetchall()
+    except Exception:
+        payload = "Invalid Query"
     conn.close()
     return payload
 
